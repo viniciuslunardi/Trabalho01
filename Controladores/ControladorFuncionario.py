dabcd11c8178165057573b7aeb14f7130c11bd3b
viniciuslunardi <Aplicativo10>
2019-10-08 23:10:51 -0300

    Criando exception na hora de cadastrar cargo de funcionario

--- Controladores/ControladorFuncionario.py
+++ Controladores/ControladorFuncionario.py
@@ -1,12 +1,14 @@
 from Telas.TelaFuncionario import TelaFuncionario
 from Entidades.Funcionario import Funcionario
 from Entidades.Funcionario import Cargo
+from Controladores.ControladorVeiculo import ControladorVeiculo
 #PARA VEÍCULOS, PRECISAREMOS USAR A CLASSE DE VEÍCULOS :)
 
 class ControladorFuncionario:
     def __init__(self):
         self.__tela_funcionario = TelaFuncionario(self)
         self.__funcionarios = []
+        self.__cargo = None
 
     def inicia(self):
         self.abre_tela_inicial()
@@ -29,8 +31,28 @@ class ControladorFuncionario:
             print("-----------------ATENÇÃO----------------- \n * Funcionário já cadastrado * ")
 
     def cadastra(self):
+        cargo = {1: Cargo.DIRETORIA, 2: Cargo.RH, 3: Cargo.OPERARIO}
         self.cadastrar_funcionario(input("NÚMERO MATRICULA: "), input("NOME: "), input("DATA NASC: "), input("TELEFONE"),
-                               input("VEICULOS: "), Cargo(int(input("CARGO: 1 2 OU 3: ")))) #mostrar opcoes de cargo
+                               input("VEICULOS: "),
+                                   Cargo(self.cadastrar_cargo())) #mostrar opcoes de cargo
+
+    def cadastrar_cargo(self):
+        cargo = {1: Cargo.DIRETORIA, 2: Cargo.RH, 3: Cargo.OPERARIO}
+        numeros_validos = [1, 2, 3]
+        while True:
+            entrada = input("CARGO: \n  * 1 = DIRETORIA \n  * 2 = RH \n  * 3 = OPERARIO \n")
+            try:
+                inteiro = int(entrada)
+                if numeros_validos and inteiro not in numeros_validos:
+                    raise ValueError
+                else:
+                    self.__cargo = Cargo(cargo[inteiro])
+                return inteiro
+            except ValueError:
+                print("Valor incorreto")
+                print("Valores validos: ", numeros_validos)
+
+
 
     def lista_funcionario(self):
         for funcionario in self.__funcionarios:

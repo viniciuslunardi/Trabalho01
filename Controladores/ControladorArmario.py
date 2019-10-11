703a8fcb1bfa8755095489d0f8b783a6e10c07ad
viniciuslunardi <Aplicativo10>
2019-10-11 19:17:20 -0300

    corrigindo bugs ao emprestar veiculo

--- Controladores/ControladorArmario.py
+++ Controladores/ControladorArmario.py
@@ -9,14 +9,13 @@ class ControladorArmario:
         self.__tela_armario = TelaArmario(self)
         self.__chaves = {}
         self.__chaves_emprestadas = {}
-        self.__chaves_disponiveis = []
         self.__controlador_principal = controlador_principal
 
     def abre_armario(self):
         self.abre_tela_inicial()
 
     def abre_tela_inicial(self):
-        switcher = {1: self.veiculos_na_garagem, 2: self.pegar_veiculo}
+        switcher = {0: self.voltar, 1: self.veiculos_na_garagem, 2: self.pegar_veiculo, 4: self.veiculos_emprestados}
         while True:
             opcao = self.__tela_armario.mostrar_opcoes()
             funcao_escolhida = switcher[opcao]
@@ -28,11 +27,13 @@ class ControladorArmario:
 
     def veiculos_na_garagem(self):
         veiculos = self.__controlador_principal.controlador_veiculo.veiculos
-        print("--------------GARAGEM--------------")
-
-        for veiculo in veiculos:
-            if veiculo not in self.__chaves_emprestadas:
-                print("MODELO: %s PLACA: %s" % (veiculos[veiculo].modelo, veiculos[veiculo].placa))
+        print("---------------GARAGEM---------------")
+        if len(self.__chaves_emprestadas) == len(veiculos):
+            print("Nenhum veículo na garem no momento")
+        else:
+            for veiculo in veiculos:
+                if veiculo not in self.__chaves_emprestadas:
+                    print("MODELO: %s PLACA: %s" % (veiculos[veiculo].modelo, veiculos[veiculo].placa))
 
     def pegar_veiculo(self):
         self.veiculos_na_garagem()
@@ -68,35 +69,52 @@ class ControladorArmario:
                         else:
                             # EMITIR REGISTRO ACESSO NEGADO
                             print("Esse veículo não está disponível no momento")
-                    elif len(funcionario) == 1:
-                        # EMITIR REGISTRO ACESSO PERMITIDO
-                        print("Retire seu veiculo")
+                    elif len(veiculos_funcionario) == 1:
+                        if self.__chaves_emprestadas == {}:
+                            # EMITIR REGISTRO ACESSO PERMITIDO
+                            #self.__chaves_emprestadas.keys() = veiculos_funcionario[placa]
+                            placa = list(veiculos_funcionario)
+                            chave = veiculos_funcionario[placa[0]].chave
+                            self.__chaves_emprestadas[chave] = veiculos_funcionario[placa[0]]
+                            #self.__chaves_emprestadas[chave] = veiculos_funcionario[placa]
+                            print("Retire seu veiculo")
                     else:
-                        print("Funcionário não tem acesso  nenhum carro")
+                        print("Funcionário não tem acesso a nenhum carro")
+                elif len(veiculos_garagem) == 1:
+                    if self.__chaves_emprestadas == {}:
+                        # EMITIR REGISTRO ACESSO PERMITIDO
+                        placa = list(veiculos_garagem)
+                        chave = veiculos_garagem[placa[0]].chave
+                        self.__chaves_emprestadas[chave] = veiculos_garagem[placa[0]]
+                        print("Retire seu veiculo")
+                        return
+                    else:
+                        print("Veículo não está disponível no momento")
                 else:
                     print("Não existe nenhum veículo na garagem")
         if tentativas == 3:
             # EMITIR EVENTO ACESSO BLOQUEADO
             print("Acesso bloqueado")
 
-def devolver_chave(self):
-    pass
+    def veiculos_emprestados(self):
+        veiculos = self.__controlador_principal.controlador_veiculo.veiculos
+        print("---------VEÍCULOS EMPRESTADOS--------")
+        for veiculo in veiculos:
+            if veiculo in self.__chaves_emprestadas:
+                print("MODELO: %s PLACA: %s" % (veiculos[veiculo].modelo, veiculos[veiculo].placa))
+
+    def devolver_chave(self):
+        pass
 
 
-@property
-def chaves(self):
-    return self.__chaves
+    @property
+    def chaves(self):
+        return self.__chaves
 
 
-@property
-def chaves_emprestadas(self):
-    return self.__chaves_emprestadas
+    @property
+    def chaves_emprestadas(self):
+        return self.__chaves_emprestadas
 
-
-@property
-def chaves_disponiveis(self):
-    return self.__chaves_disponiveis
-
-
-def voltar(self):
-    self.__controlador_principal.abre_tela_inicial()
+    def voltar(self):
+        self.__controlador_principal.abre_tela_inicial()

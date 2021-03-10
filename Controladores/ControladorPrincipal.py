d52e6bded414888667c1b7092df9f09039886485
Mércia Maguerroski <merciasmc@outlook.com>
2021-03-10 00:34:20 -0300

    merge

--- Controladores/ControladorPrincipal.py
+++ Controladores/ControladorPrincipal.py
@@ -1,12 +1,16 @@
 from Telas.TelaPrincipal import TelaPrincipal
+from Telas.TelaLogin import TelaLogin
 from Controladores.ControladorFuncionario import ControladorFuncionario
 from Controladores.ControladorAluno import ControladorAluno
+from Entidades.src.FuncionarioDAO import FuncionarioDAO
 
 class ControladorPrincipal:
     __instance = None
 
     def __init__(self):
         self.__tela_principal = TelaPrincipal(self)
+        self.__tela_login = TelaLogin(self)
+        self.__funcionarios_DAO = FuncionarioDAO()
         self.__controlador_funcionario = ControladorFuncionario(self)
         self.__controlador_aluno = ControladorAluno(self)
 
@@ -16,12 +20,27 @@ class ControladorPrincipal:
             return ControladorPrincipal.__instance
 
     def inicia(self):
-        self.abre_tela_inicial()
+        self.abre_tela_login()
 
-    def abre_tela_inicial(self):
-        options = {0: self.funcionario, 1: self.aluno}
-        button, values = self.__tela_principal.open()
-        return options[button]()
+    def abre_tela_login(self):
+        button, values = self.__tela_login.open()
+
+        usuario = values[0]
+        senha = values[1]
+        if not self.__funcionarios_DAO.get(usuario):
+            rec_senha = self.__funcionarios_DAO.get(usuario).password
+            if rec_senha == senha:
+                return self.abre_tela_inicial()
+            else:
+                self.__tela_login.show_message("Erro",
+                                               "Senha incorreta")
+                return self.abre_tela_login()
+        else:
+            print("Não existe usuario com esse login cadastrado no sistema")
+            self.__tela_login.show_message("Erro",
+                                           "Não existe usuario com esse login cadastrado no sistema")
+            return self.abre_tela_login()
+
 
     @property
     def controlador_funcionario(self):
@@ -30,6 +49,11 @@ class ControladorPrincipal:
     @property
     def controlador_aluno(self):
         return self.__controlador_aluno
+
+    def abre_tela_inicial(self):
+        options = {0: self.funcionario, 1: self.aluno}
+        button, values = self.__tela_principal.open()
+        return options[button]()
 
     def funcionario(self):
         self.__controlador_funcionario.abre_funcionario()
